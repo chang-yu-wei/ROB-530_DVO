07b988f348516c7ffd13c16d967eee8f11ae9ec7
Justin Lin <tzuyuan@umich.edu>
2019-04-23 21:57:14 -0400

    add some files

--- evaluate/evaluate_rpe.sh
+++ evaluate/evaluate_rpe.sh
@@ -1,7 +1,7 @@
 # for rpe
 # python3 evaluate_ate.py groundtruth.txt trajectory.txt --fixed_delta --verbose --plot plot_path --title for plotting --save path_for saving results
-python3 evaluate_rpe.py ../data/traj_log/fr2_desk/groundtruth.txt ../data/traj_log/fr2_desk/dvo_fr2_desk_t.txt --fixed_delta --verbose --plot ../data/traj_log/fr2_desk/dvo_rpe.png --title 'DVO' --save '../data/traj_log/fr2_desk/dvo_err.txt'
-python3 evaluate_rpe.py ../data/traj_log/fr2_desk/groundtruth.txt ../data/traj_log/fr2_desk/BA_full_t.txt --fixed_delta --verbose --plot ../data/traj_log/fr2_desk/BA_full_rpe.png --title 'Local BA' --save '../data/traj_log/fr2_desk/ba_err.txt'
-python3 evaluate_rpe.py ../data/traj_log/fr2_desk/groundtruth.txt ../data/traj_log/fr2_desk/Pose_graph_full_t.txt --fixed_delta --verbose --plot ../data/traj_log/fr2_desk/Pose_graph_full_rpe.png --title 'Global BA' --save '../data/traj_log/fr2_desk/pose_graph_err.txt'
-python3 evaluate_rpe.py ../data/traj_log/fr2_desk/groundtruth.txt ../data/traj_log/fr2_desk/orb_slam.txt --fixed_delta --verbose --plot ../data/traj_log/fr2_desk/orb_slam_rpe.png --title 'ORBSLAM' --save '../data/traj_log/fr2_desk/orbslam_err.txt'
+python3 evaluate_rpe.py ../data/traj_log/groundtruth.txt ../data/traj_log/dvo_fr2_desk_t.txt --fixed_delta --verbose --plot ../data/traj_log/dvo_rpe.png --title 'DVO' --save '../data/traj_log/dvo_err.txt'
+python3 evaluate_rpe.py ../data/traj_log/groundtruth.txt ../data/traj_log/BA_full_t.txt --fixed_delta --verbose --plot ../data/traj_log/BA_full_rpe.png --title 'Local BA' --save '../data/traj_log/ba_err.txt'
+python3 evaluate_rpe.py ../data/traj_log//groundtruth.txt ../data/traj_log/Pose_graph_full_t.txt --fixed_delta --verbose --plot ../data/traj_log/Pose_graph_full_rpe.png --title 'Global BA' --save '../data/traj_log/pose_graph_err.txt'
+python3 evaluate_rpe.py ../data/traj_log/groundtruth.txt ../data/traj_log/orb_slam.txt --fixed_delta --verbose --plot ../data/traj_log/orb_slam_rpe.png --title 'ORBSLAM' --save '../data/traj_log/orbslam_err.txt'
 
